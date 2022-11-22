27fc4442825329e5f4142becd3c2fb28d0cd63a5
Zehra Tok <zehratok1@outlook.com>
2022-11-22 23:01:52 +0300

    Complete the main screen. (Home Screen)

--- nobetci_eczaneler/src/screens/Home/Home.js
+++ nobetci_eczaneler/src/screens/Home/Home.js
@@ -1,13 +1,172 @@
-import { View, Text, ScrollView } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { FlatList, SafeAreaView, StatusBar, Text, TouchableOpacity, View } from 'react-native'
+import { Header } from '../../components'
+import { red } from '../../style/colors'
+import SelectDropdown from 'react-native-select-dropdown'
+import cities from '../../utils/cities'
 import styles from './Home.style'
 
 const Home = () => {
+  const [selectedCity, setSelectedCity] = useState(''); // şehir seçilince index değeri buraya yazılacak
+  const [town, setTown] = useState('Konum Seçiniz');  // header'da gösterilecek ve api isteği için kullanılacak
+  const [district, setDistrict] = useState(''); // header'da gösterilecek ve api isteği için kullanılacak
+  const [pharmacies, setPharmacies] = useState([]); // api'den gelen eczane data'sı buraya yazılacak
+  const apiUrl = 'https://www.nosyapi.com/apiv2/pharmacy';
+
+  useEffect(() => {
+    if (town !== 'Konum Seçiniz' && district === '') { // şehir seçilmişse 
+      handleGetPharmaciesByCity(); // seçilen şehre göre eczaneleri getir
+    }
+    else if (town !== 'Konum Seçiniz' && district !== '') { // ilçe seçilmişse
+      handleGetPharmaciesByCounty(); // seçilen ilçeye göre eczaneleri getir
+    }
+    else { // şehir seçilmemişse
+      handleGetPharmacies(); // tüm eczaneleri getir
+    }
+  }, [town]); // component mount olduğunda ve town değiştiğinde çalıştır
+
+  useEffect(() => {
+    handleGetPharmaciesByCounty(); // seçilen ilçede eczaneleri getir
+  }, [district]) // ilçe seçildiğinde çağır
+
+  handleGetPharmacies = () => { // tüm eczaneleri getirme fonksiyonu
+    fetch(apiUrl + '/getAll', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'API_KEY' // noseyapi.com'dan alınacak tokenı API_KEY yerine yazın
+      }
+    })
+      .then((response) => response.json()) // json'a çevir
+      .then((json) => {
+        setPharmacies(json.data); // pharmacies state'ini güncelle
+      }
+      )
+      .catch((error) => {
+        console.error(error);
+      }
+      );
+  }
+  handleGetPharmaciesByCity = () => { // şehre göre eczaneleri getirme fonksiyonu
+    fetch(apiUrl + `?city=${town.toLowerCase()}`, {  //api küçük harfli değerleri istediği için küçük harfe çevir
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'API_KEY' // noseyapi.com'dan alınacak tokenı API_KEY yerine yazın
+      }
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        setPharmacies(json.data);
+      }
+      )
+      .catch((error) => {
+        console.error(error);
+      }
+      );
+  }
+  handleGetPharmaciesByCounty = () => { // ilçeye göre eczaneleri getirme fonksiyonu
+    fetch(apiUrl + `?city=${town.toLowerCase()}&county=${district.toLowerCase()}`, { //api küçük harfli değerleri istediği için küçük harfe çevir
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'API_KEY' //noisyapi.com'dan alınacak tokenı API_KEY yerine yazın
+      }
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        setPharmacies(json.data);
+      }
+      )
+      .catch((error) => {
+        console.error(error);
+      }
+      );
+  }
   return (
-    <ScrollView style={styles.container}>
-      <Text style={styles.text}>Home</Text>
-    </ScrollView>
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor={red} />
+      <View style={styles.headerContainer}>
+        <Header text='Nöbetçi Eczaneler' />
+        <Text style={styles.headerDistrict}> {district} </Text>
+        <Text style={styles.headerTown}> {town} </Text>
+      </View>
+      <View style={styles.selectContainer}>
+        <SelectDropdown
+          data={city = cities.map((city) => (city.name).toUpperCase())} //şehirleri, kendim oluşturduğum cities.js dosyasından çek ve büyük harfe çevir
+          defaultButtonText='Şehir Seçiniz'
+          onSelect={(selectedItem, index) => { // şehir seçildiğinde
+            setSelectedCity(index) // şehir index değerini selectedCity state'ine yaz
+            setTown(selectedItem) // şehir state'ini seçilen item olarak güncelle
+          }}
+          buttonStyle={styles.selectButton}
+          buttonTextStyle={styles.selectButtonText}
+          renderDropdownIcon={() => {
+            return (
+              <View style={styles.selectIconContainer}>
+                <Text style={styles.selectIcon}>▼</Text>
+              </View>
+            )
+          }}
+          dropdownStyle={styles.selectDropdown}
+          rowStyle={styles.selectRow}
+          rowTextStyle={styles.selectRowText}
+        />
+      </View>
+      <View style={styles.selectContainer2}>
+        {
+          selectedCity !== '' ? ( // şehir seçilmişse ilçeleri göster
+            <SelectDropdown
+              data={county = (cities[selectedCity].counties).map((county) => (county).toUpperCase())} // seçilen şehrin ilçelerini, kendim oluşturduğum cities.js dosyasından çek ve büyük harfe çevir
+              defaultButtonText='İlçe Seçiniz'
+              onSelect={(selectedItem, index) => {
+                setDistrict(selectedItem)
+              }}
+              buttonStyle={styles.selectButton}
+              buttonTextStyle={styles.selectButtonText}
+              renderDropdownIcon={() => {
+                return (
+                  <View style={styles.selectIconContainer}>
+                    <Text style={styles.selectIcon}>▼</Text>
+                  </View>
+                )
+              }}
+              dropdownStyle={styles.selectDropdown}
+              rowStyle={styles.selectRow}
+              rowTextStyle={styles.selectRowText}
+            />
+          ) : (
+            <SelectDropdown
+              data={county = ['Lütfen önce şehir seçiniz']} // şehir seçilmemişse ilçe seçimini engelle
+              defaultButtonText='İlçe Seçiniz'
+              disabled={true} // seçim yapılmasını engelle çünkü şehir seçilmemiş
+              buttonStyle={styles.selectButton}
+              buttonTextStyle={styles.selectButtonText}
+              renderDropdownIcon={() => {
+                return (
+                  <View style={styles.selectIconContainer}>
+                    <Text style={styles.selectIcon}>▼</Text>
+                  </View>
+                )
+              }}
+              dropdownStyle={styles.selectDropdown}
+              rowStyle={styles.selectRow}
+              rowTextStyle={styles.selectRowText}
+            />
+          )
+        }
+      </View>
+      <FlatList data={pharmacies} renderItem={({ item }) => ( // pharmacies state'ini flatlist ile göster
+        <TouchableOpacity style={styles.pharmacyCard}>
+          <Text style={styles.pharmacyName}>{item.EczaneAdi}</Text>
+          <Text style={styles.pharmacyAddress}>{item.Adresi}</Text>
+          <Text style={styles.pharmacyCounty}>{item.Semt}</Text>
+          <Text style={styles.pharmacyAddress2}>{item.YolTarifi}</Text>
+          <Text style={styles.pharmacyPhone}>{item.Telefon}  {item.Telefon2}</Text>
+        </TouchableOpacity>
+      )}
+        keyExtractor={item => item.id} // her bir eczanenin id'sini key olarak kullan
+      />
+    </SafeAreaView>
   )
 }
-
 export default Home
--- nobetci_eczaneler/src/screens/Home/Home.style.js
+++ nobetci_eczaneler/src/screens/Home/Home.style.js
@@ -1,6 +1,133 @@
 import { StyleSheet } from "react-native";
+import { red, transparent, white } from "../../style/colors";
+import { height, width, SfontSize, MfontSize, LfontSize, XLfontSize } from "../../style/dimensions";
+
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    headerContainer: {
+        height: height * 0.1,
+        width: width,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    headerDistrict: {
+        position: 'absolute',
+        fontSize: XLfontSize,
+        fontWeight: 'bold',
+        color: red,
+        left: 0.01 * width,
+        top: 0.08 * height,
+    },
+    headerTown: {
+        position: 'absolute',
+        fontSize: XLfontSize,
+        fontWeight: 'bold',
+        color: red,
+        left: 0.01 * width,
+        top: 0.12 * height,
+    },
+    selectContainer: {
+        marginTop: 0.05 * height,
+        height: height * 0.1,
+        width: width,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    selectContainer2: {
+        marginBottom: 0.02 * height,
+    },
+    selectButton: {
+        width: width * 0.8,
+        height: height * 0.05,
+        backgroundColor: white,
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignSelf: 'center',
+    },
+    selectButtonText: {
+        color: red,
+        fontSize: 20,
+    },
+    selectIconContainer: {
+        width: width * 0.1,
+        height: height * 0.05,
+        backgroundColor: red,
+        borderTopRightRadius: 10,
+        borderBottomRightRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        left: - 0.02 * width,
+    },
+    selectIcon: {
+        color: white,
+        fontSize: 20,
+    },
+    selectDropdown: {
+        width: width * 0.8,
+        height: height * 0.3,
+        backgroundColor: white,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: red,
+    },
+    selectRow: {
+        width: width * 0.8,
+        height: height * 0.05,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderBottomWidth: 1,
+        borderBottomColor: red,
+    },
+    selectRowText: {
+        color: red,
+        fontSize: 20,
+    },
+    pharmacyCard: {
+        width: width * 0.85,
+        minHeight: height * 0.2,
+        maxHeight: height * 0.3,
+        backgroundColor: white,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: red,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginVertical: 0.01 * height,
+        alignSelf: 'center',
+        padding: 0.01 * height,
+    },
+    pharmacyName: {
+        textAlign: 'center',
+        fontSize: XLfontSize,
+        fontWeight: 'bold',
+        color: red,
+    },
+    pharmacyAddress: {
+        textAlign: 'center',
+        fontSize: LfontSize,
+        color: red,
+    },
+    pharmacyCounty: {
+        textAlign: 'center',
+        fontSize: LfontSize,
+        color: red,
+    },
+    pharmacyPhone: {
+        textAlign: 'center',
+        fontSize: LfontSize,
+        color: red,
+    },
+    pharmacyAddress2: {
+        textAlign: 'center',
+        fontSize: MfontSize,
+        color: red,
+    },
+
+
+
 
 });
 export default styles;
