a436f70998fcc841617e7c10f19d68785e48de4b
Zehra Tok <zehratok1@outlook.com>
2022-11-22 23:02:51 +0300

    Complete the about screen.

--- nobetci_eczaneler/src/screens/About/About.js
+++ nobetci_eczaneler/src/screens/About/About.js
@@ -1,11 +1,26 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import styles from './About.style'
+import { Header } from '../../components'
 
 const About = () => {
   return (
-    <View>
-      <Text>About</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Header text='Hakkında' />
+      <View style={styles.aboutCard} >
+        <Image source={require('../../assets/images/marker.png')} style={styles.image} />
+        <Text style={styles.textHeader}> Nöbetçi Eczaneler uygulaması, </Text>
+        <Text style={styles.text}> Türkiye'deki nöbetçi eczaneleri göstermektedir. </Text>
+        <Text style={styles.text}> Uygulama içerisinde bulunan veriler, </Text>
+        <Text style={styles.text}> NosyApi üzerinden alınmaktadır. </Text>
+        <View style={styles.footer}>
+          <Text style={styles.textFooter}> React Native ile geliştirilmiştir. </Text>
+          <Text style={styles.textFooter}> @zehratok Kasım,2022 </Text>
+        </View>
+      </View>
+    </SafeAreaView >
+
   )
 }
 
--- nobetci_eczaneler/src/screens/About/About.style.js
+++ nobetci_eczaneler/src/screens/About/About.style.js
@@ -1,8 +1,62 @@
 import { StyleSheet } from "react-native";
+import { red, transparent, white } from "../../style/colors";
+import { height, width, SfontSize, MfontSize, LfontSize, XLfontSize } from "../../style/dimensions";
 
 const styles = StyleSheet.create({
-
-
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    aboutCard: {
+        width: width * 0.9,
+        height: height * 0.6,
+        backgroundColor: white,
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: width * 0.05,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 10,
+    },
+    image: {
+        width: width * 0.3,
+        height: height * 0.2,
+        marginBottom: height * 0.05,
+    },
+    textHeader: {
+        fontSize: XLfontSize,
+        fontWeight: 'bold',
+        color: red,
+        marginBottom: 0.02 * height,
+    },
+    text: {
+        fontSize: MfontSize,
+        color: red,
+    },
+    textBold: {
+        fontSize: MfontSize,
+        color: red,
+        fontWeight: 'bold',
+    },
+    footer: {
+        flexDirection: 'column',
+        justifyContent: 'space-evenly',
+        width: width * 0.8,
+        marginTop: 0.05 * height,
+    },
+    textFooter: {
+        textAlign: 'center',
+        fontSize: MfontSize,
+        color: red,
+        fontStyle: 'italic',
+    },
 
 });
 export default styles;
